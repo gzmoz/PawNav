222fcbba091c883052193ae96129c9af478fa16f
GizemOz <ozgzm2001@gmail.com>
2025-03-05 01:09:28 +0300

    commit

--- app/src/main/java/com/example/pawnav/MapsFragment.kt
+++ app/src/main/java/com/example/pawnav/MapsFragment.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.pawnav.databinding.FragmentMapsBinding
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -16,32 +17,53 @@ import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsFragment : Fragment() {
 
+    private lateinit var binding: FragmentMapsBinding
+    private var isSatelliteView = false // Default: Normal map view
+
+    /*This function is called when the Google Map is ready.
+    You can add markers, move the camera, or set event listeners inside this function.*/
     private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        val sydney = com.google.android.gms.maps.model.LatLng(-34.0, 151.0)
+        // Google Maps UI
+        googleMap.uiSettings.isZoomGesturesEnabled = true
+        googleMap.uiSettings.isScrollGesturesEnabled = true
+        googleMap.uiSettings.isZoomControlsEnabled = true
+
+        val sydney = LatLng(-34.0, 151.0)
         googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15f))
+
+        // Set up toggle button for map type switching
+        binding.btnToggleMap.setOnClickListener{
+            isSatelliteView = !isSatelliteView //toggle boolean
+
+            if(isSatelliteView){
+                googleMap.mapType = GoogleMap.MAP_TYPE_SATELLITE
+                binding.btnToggleMap.setImageResource(R.drawable.map_icon)
+            }else{
+                googleMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+                binding.btnToggleMap.setImageResource(R.drawable.satellite_icon)
+            }
+        }
+
     }
 
+    /*This function is responsible for creating and returning the view for the fragment.
+    It inflates the fragment_maps.xml layout.*/
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_maps, container, false)
+        binding = FragmentMapsBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
+    /*This function is called after the view has been created.
+    It is used to initialize UI elements and perform setup tasks.*/
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
     }
+
 }
